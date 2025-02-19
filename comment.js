16d214f45aaabf5bba1524af02e2400d0fb7cfad
John Dara <37872957+Jayozone1@users.noreply.github.com>
2025-02-19 18:00:20 +0000

    Copilot third commit

---
+++ comment.js
@@ -0,0 +1,41 @@
+//Create Web Server
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+// Get all comments
+app.get('/listComments', function (req, res) {
+    fs.readFile( __dirname + "/" + "comments.json", 'utf8', function (err, data) {
+        console.log( data );
+        res.end( data );
+    });
+})
+
+// Add a comment
+app.get('/addComment', function (req, res) {
+    // First read existing comments.
+    fs.readFile( __dirname + "/" + "comments.json", 'utf8', function (err, data) {
+        data = JSON.parse( data );
+        data["comment4"] = req.query.comment;
+        console.log( data );
+        res.end( JSON.stringify(data));
+    });
+})
+
+// Delete a comment
+app.get('/deleteComment', function (req, res) {
+    // First read existing comments.
+    fs.readFile( __dirname + "/" + "comments.json", 'utf8', function (err, data) {
+        data = JSON.parse( data );
+        delete data["comment4"];
+        console.log( data );
+        res.end( JSON.stringify(data));
+    });
+})
+
+// Start server
+var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Comment App listening at http://%s:%s", host, port)
+})
